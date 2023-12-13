1d471ec3c428934f0bef1c304a874c56e414a6b7
Abel Pujol <abelpujol99@gmail.com>
2023-12-13 20:54:40 +0100

    Visual Initialized

---
+++ AppCompanion/app/src/main/java/cdi/interfacedesign/lolrankedtracker/activities/MainActivity.kt
@@ -0,0 +1,14 @@
+package cdi.interfacedesign.lolrankedtracker.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import cdi.interfacedesign.lolrankedtracker.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.screen_main_activity)
+    }
+}
